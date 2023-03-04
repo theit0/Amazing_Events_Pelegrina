2cbb9654218bd036058088f25ba47ed77a606d51
theit0 <theopelegrina469@gmail.com>
2023-03-04 19:49:55 -0300

    banners created

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,24 +1,24 @@
 const $toggleSwitch = document.querySelectorAll('.input-switch'),
       $card = document.querySelectorAll(".card"),
-      $h6 = document.querySelectorAll("h6"),
+      $titlePrice = document.querySelectorAll(".title-price"),
       $h4 = document.querySelectorAll("h4"),
       $a = document.querySelectorAll("a"),
       $main = document.querySelector("main"),
       $svg = document.querySelectorAll("svg"),
-      $h1 = document.querySelector("h1"),
       $table = document.querySelector("table");
 
 
 $toggleSwitch.forEach(element => {
     element.addEventListener('click',() => {
-        $h4.forEach(elem=> elem.classList.toggle("dark-text"));
-        $h6.forEach(elem=>elem.classList.toggle('dark-text'));
+        $h4.forEach(elem=> elem.classList.toggle("white-text"));
+        $titlePrice.forEach(elem=>elem.classList.toggle("white-text"));
         $card.forEach(elem => elem.classList.toggle("dark-card"));
-        $svg.forEach(elem =>elem.classList.toggle("dark-text"));
+        $svg.forEach(elem =>elem.classList.toggle("white-text"));
         document.body.classList.toggle("dark-card");
         $main.classList.toggle('dark-main');
-        $h1.classList.toggle("dark-text");
         $table.classList.toggle("dark-card");
+
+
     });
 }); 
 
