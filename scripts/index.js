89c8f08f798a60ba6c506df9988807eac57b8287
theit0 <theopelegrina469@gmail.com>
2023-03-07 12:03:22 -0300

    fix pending

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,79 @@
 const $templateCard = document.querySelector(".templateCard").content,
       $cards = document.querySelector(".cards"),
-      $fragment = document.createDocumentFragment();
+      $fragment = document.createDocumentFragment(),
+      $checkboxes = document.querySelectorAll(".categories input[type='checkbox']");
 
-data.events.forEach((elem)=>{
+
+
+console.log($checkboxes);
+
+/* const arrayOfNodes = Array.from($checkboxes);
+const categoriesChecked = arrayOfNodes.filter((checkbox)=>checkbox.checked);
+const categoriesNames = categoriesChecked.map((category)=> category.name);
+
+
+console.log(categoriesNames);
+
+const eventsFiltered = data.events.filter((event)=>{
+    console.log(event.category);
+    return categoriesNames.includes(event.category); 
+}) 
+ */
+function filtrarEventos () {
+    const arrayOfNodes = Array.from($checkboxes);
+    const categoriesChecked = arrayOfNodes.filter((checkbox)=>checkbox.checked);
+    const categoriesNames = categoriesChecked.map((category)=> category.name);
+    
+    
+    console.log(categoriesNames);
+    
+    const eventsFiltered = data.events.filter((event)=>{
+        console.log(event.category);
+        return categoriesNames.includes(event.category); 
+    });
+    eventsFiltered.forEach((elem)=>{
+        $templateCard.querySelector("img").setAttribute("src",elem.image);
+        $templateCard.querySelector("img").setAttribute("alt",elem.name);
+        $templateCard.querySelector("h4").textContent = elem.name;
+        $templateCard.querySelector("p").textContent = elem.description;
+        $templateCard.querySelector("h6").textContent = `Price: $${elem.price}`;
+    
+        
+        let $clone = document.importNode($templateCard,true);
+        //Uso de fragment para no tener que agregar un elemento por vez
+        // y de esta forma mejor el rendimiento de la página
+        $fragment.append($clone);
+    });
+    $cards.append($fragment);
+    console.log($cards.querySelectorAll(".card"))
+}
+
+function limpiarEventos(cards) {
+    
+}
+
+for (let i of $checkboxes) {
+    i.addEventListener('change',()=>{
+        filtrarEventos();
+        
+        /* eventsFiltered.forEach((elem)=>{
+            $templateCard.querySelector("img").setAttribute("src",elem.image);
+            $templateCard.querySelector("img").setAttribute("alt",elem.name);
+            $templateCard.querySelector("h4").textContent = elem.name;
+            $templateCard.querySelector("p").textContent = elem.description;
+            $templateCard.querySelector("h6").textContent = `Price: $${elem.price}`;
+        
+            
+            let $clone = document.importNode($templateCard,true);
+            //Uso de fragment para no tener que agregar un elemento por vez
+            // y de esta forma mejor el rendimiento de la página
+            $fragment.append($clone);
+        }) */
+    })
+    console.log(i);
+}
+
+/* eventsFiltered.forEach((elem)=>{
     $templateCard.querySelector("img").setAttribute("src",elem.image);
     $templateCard.querySelector("img").setAttribute("alt",elem.name);
     $templateCard.querySelector("h4").textContent = elem.name;
@@ -14,6 +85,7 @@ data.events.forEach((elem)=>{
     //Uso de fragment para no tener que agregar un elemento por vez
     // y de esta forma mejor el rendimiento de la página
     $fragment.append($clone);
-})
+}) */
 
-$cards.append($fragment);
+/* $cards.append($fragment);
+ */
