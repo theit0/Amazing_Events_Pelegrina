46c715db4a341fdfd2d68e29bdc2846c86597134
theit0 <theopelegrina469@gmail.com>
2023-03-07 23:31:35 -0300

    show all events button added

--- scripts/index.js
+++ scripts/index.js
@@ -5,20 +5,10 @@ const $templateCard = document.querySelector(".templateCard").content,
       
 console.log($checkboxes);
 
-function filtrarEventos () {
-    const arrayOfNodes = Array.from($checkboxes);
-    const categoriesChecked = arrayOfNodes.filter((checkbox)=>checkbox.checked);
-    const categoriesNames = categoriesChecked.map((category)=> category.name);
-     
-    
-    
-    console.log(categoriesNames);
-    
-    const eventsFiltered = data.events.filter((event)=>{
-        console.log(event.category);
-        return categoriesNames.includes(event.category); 
-    });
-    eventsFiltered.forEach((elem)=>{
+const $showAllButton = document.querySelector(".show-events-button");
+
+$showAllButton.addEventListener('click',()=>{
+    data.events.forEach((elem)=>{
         $templateCard.querySelector("img").setAttribute("src",elem.image);
         $templateCard.querySelector("img").setAttribute("alt",elem.name);
         $templateCard.querySelector("h4").textContent = elem.name;
@@ -32,10 +22,36 @@ function filtrarEventos () {
         $fragment.append($clone);
     });
     $cards.append($fragment);
+    $cards.removeChild($cards.querySelector(".show-events-container"));
+});
+    
+
+
+function filtrarEventos () {
+    const $arrayOfNodes = Array.from($checkboxes);
+    const $categoriesChecked = $arrayOfNodes.filter((checkbox)=>checkbox.checked);
+    const $categoriesNames = $categoriesChecked.map((category)=> category.name);
+     
+    console.log($categoriesNames);
+    
+    const eventsFiltered = data.events.filter((event)=>{
+        console.log(event.category);
+        return $categoriesNames.includes(event.category); 
+    });
+    eventsFiltered.forEach((elem)=>{
+        $templateCard.querySelector("img").setAttribute("src",elem.image);
+        $templateCard.querySelector("img").setAttribute("alt",elem.name);
+        $templateCard.querySelector("h4").textContent = elem.name;
+        $templateCard.querySelector("p").textContent = elem.description;
+        $templateCard.querySelector("h6").textContent = `Price: $${elem.price}`;
+    
+        
+        let $clone = document.importNode($templateCard,true);
+        $fragment.append($clone);
+    });
+    $cards.append($fragment);
     console.log($cards.querySelectorAll(".card"))
 }
-
-
 
 for (let i of $checkboxes) {
     i.addEventListener('change',()=>{
